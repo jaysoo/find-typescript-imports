022d1abef9fb6d7cfb38415be775f9fad9229bad
Jack Hsu <jack.hsu@gmail.com>
2023-06-12 11:35:01 -0400

    cleanup

--- src/lib.rs
+++ src/lib.rs
@@ -7,6 +7,7 @@ extern crate swc_ecma_parser;
 extern crate swc_ecma_visit;
 extern crate swc_ecma_ast;
 
+use std::io::{Error, ErrorKind};
 use futures::stream::StreamExt;
 use tokio::fs::read_to_string;
 use swc_common::{
@@ -36,9 +37,9 @@ impl Visit for ImportVisitor {
 
 async fn process_file(file_path: String) -> Vec<String> {
     let content = read_to_string(&file_path).await;
-    let file_path_2 = file_path.clone();
-    match content {
-      Ok(source) => {
+
+    let result = content
+      .and_then(|source| {
         let cm: Lrc<SourceMap> = Default::default();
         let fm = cm.new_source_file(FileName::Real(file_path.into()), source.clone());
 
@@ -51,28 +52,26 @@ async fn process_file(file_path: String) -> Vec<String> {
           StringInput::from(&*fm),
           None,
         );
+        let mut parser =  Parser::new_from(lexer);
 
-        let mut parser = Parser::new_from(lexer);
+        let mut visitor = ImportVisitor { imports: vec![] };
+        let module = parser.parse_module()
+          .map_err(|_| Error::new(ErrorKind::Other, "Failed to parse file"))?;
+        module.visit_with(&mut visitor);
 
-        let result = parser.parse_module();
-        match result {
-          Ok(module) => {
-            let mut visitor = ImportVisitor { imports: vec![] };
-            module.visit_with(&mut visitor);
-
-            let mut file_imports: Vec<String> = vec![];
-            for import_decl in visitor.imports {
-                let span = import_decl.span;
-              let start = (span.lo().0 - 1) as usize;
-              let end = span.hi().0 as usize;
-              let import_text = source[start..end].to_string();
-              file_imports.push(format!("{}: {}", file_path_2, import_text));
-            }
-            file_imports
-          },
-          Err(_) =>  vec![]
+        let mut file_imports: Vec<String> = vec![];
+        for import_decl in visitor.imports {
+          let span = import_decl.span;
+          let start = (span.lo().0 - 1) as usize;
+          let end = span.hi().0 as usize;
+          let import_text = source[start..end].to_string();
+          file_imports.push(import_text);
         }
-      },
+        Ok(file_imports)
+      });
+
+    match result {
+      Ok(value) => value,
       Err(_) => vec![]
     }
 }
@@ -81,7 +80,7 @@ async fn process_file(file_path: String) -> Vec<String> {
 async fn find_imports(file_paths: Vec<String>) -> Vec<String>  {
   let mut file_reads: futures::stream::FuturesUnordered<_> = file_paths.into_iter().map(process_file).collect();
       
-  let mut imports : Vec<Vec<String>> = Vec::new();
+  let mut imports : Vec<Vec<String>> = vec![];
 
   while let Some(result) = file_reads.next().await {
     imports.push(result)
